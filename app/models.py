d35ed60640f1f29592dcaaf00e0634d302da7e20
ramonie <ramonmartinsmr@gmail.com>
2021-10-01 21:30:13 -0300

    criando banco

--- app/models.py
+++ app/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.db.models.base import Model
 
 # Create your models here.
-#class Cadastro(models.Model):
-    #nome = models.CharField(max_length= 100)
-    #cnpj = models.ForeignKey(max_length= 14)
-    #cep = models.CharField(max_length=9)
-    #endereco = models.CharField(max_length=100)
-    #numero = models.CharField(max_length=6)
-    #bairro = models.CharField(max_length=100)
-    #municipio = models.CharField(max_length=50)
-    #estado = models.CharField(max_length=100)
-    #email = models.EmailFieldField(max_length= 100)
-    #senha = models.CharField(max_length=50)
+class Cadastro(models.Model):
+    nome = models.CharField(max_length= 100 ) 
+    cnpj = models.CharField(max_length= 14 )
+    cep = models.CharField(max_length= 9 )
+    endereco = models.CharField(max_length= 100 ) 
+    numero = models.CharField(max_length= 6 )
+    bairro = models.CharField(max_length= 100 )
+    municipio = models.CharField(max_length= 50 )
+    estado = models.CharField(max_length= 100 )
+    email = models.CharField(max_length= 100 )
+    senha = models.CharField(max_length= 50 )
  
