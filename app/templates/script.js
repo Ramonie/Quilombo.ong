d2cf92b594bef161f85676d196f295df46bff2de
alexoliveira04 <79259398+alexoliveira04@users.noreply.github.com>
2021-10-12 23:49:48 -0300

    Add files via upload

---
+++ app/templates/script.js
@@ -0,0 +1,9 @@
+function entrar(){
+    var a = window.document.getElementById('btn')
+    a.style.background = 'rgb(167 142 107)'
+}
+function sair(){
+    var a = window.document.getElementById('btn')
+    a.style.background = 'rgb(201 170 127)'
+}
+//não ta linkando
