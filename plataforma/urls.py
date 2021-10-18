7f53a31307123022478ede6ae8923a01dcdaabc9
ramonie <ramonmartinsmr@gmail.com>
2021-10-18 15:05:03 -0300

    login

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
     path('publicar/',publicar, name='publicar'),
     path('informativo/', informativo, name='informativo'),
     path('accounts/', include('allauth.urls')),
+    
 ]
 
