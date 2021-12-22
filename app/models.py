4c2001ff8ad6f17a6ef35bc5771cda97517bb065
ramonie <ramonmartinsmr@gmail.com>
2021-12-22 20:02:25 -0300

    Salvando dados no banco com horário local

--- app/models.py
+++ app/models.py
@@ -15,7 +15,20 @@ class Cadastro(models.Model):
     senha = models.CharField(max_length= 50 )
     confsenha =  models.CharField(max_length= 50,blank=True )
     
+from django.conf import settings
+from django.utils import timezone
 
-#class Publicar(models.Model):
-    #publicacao = models.CharField(max_length=500)
-   
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- plataforma/settings.py
+++ plataforma/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
+    
    
     
 
@@ -116,13 +117,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'utc'
+TIME_ZONE = 'America/bahia'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
